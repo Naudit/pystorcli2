a08facd776c5adcc2e0e06b581c6e515f440b82e
Martin Dojcak <martin.dojcak@chillisys.com>
2018-07-27 18:06:23 +0200

    bump to 0.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setuptools.setup(
     name="pystorcli",
-    version="0.0.3",
+    version="0.0.5",
     author="Martin Dojcak",
     author_email="martin@dojcak.sk",
     description="StorCLI module wrapper",
