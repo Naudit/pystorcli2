4a78214a5deaeb114bc913b75560006ee5cc1773
Rafael Leira <rafael.leira@naudit.es>
2023-05-04 11:05:19 +0200

    added some test data

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -32,5 +32,18 @@ class TestControllers(TestStorcliMainClass):
         # List controllers
         controllers = Controllers()
 
+        assert device_data
         if "Controller_ids" in device_data:
             assert controllers.ids == device_data["Controller_ids"]
+
+    @pytest.mark.parametrize("folder", folders)
+    def test_get_controller(self, folder):
+        # setup env
+        device_data = self.setupEnv(folder)
+
+        # List controllers
+        controllers = Controllers()
+
+        assert device_data
+        if "Controller_ids" in device_data and len(controllers.ids) > 0:
+            assert controllers.get_ctl(controllers.ids.pop())
